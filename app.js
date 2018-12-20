0e7a0c02b082e0c2916f1f221964f07fd19f44a3
hema625 <41521742+hema625@users.noreply.github.com>
2018-12-20 15:49:08 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var pl = require("passport-local");
 var body = require("body-parser");
 var plm = require("passport-local-mongoose");
 var User = require("./models/user")
-
+//added for master branch
 
 var app = express();
 app.use(require("express-session")({
